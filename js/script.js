527c818ef7b86511efdb4c352f841052d5613c0b
Angelika <angelika_romanovskaya_2002@mail.ru>
2023-08-03 13:25:29 +0300

    Changes in operations

--- js/script.js
+++ js/script.js
@@ -2,18 +2,30 @@ const buttons = document.querySelector('.grid');
 const calc = document.querySelector('.calc')
 
 const number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
-const operation = ['C', '÷', '%', '←', '✕', '−', '+', '=']
+const operation = ['C', '÷', '%', '←', '×', '−', '+', '=']
 let first = '', second = '', sign= '', results;
 
 function clickNumber(key){
-    if(second === '' && sign === ''){
-        first+=key;
-        calc.value +=key;
-        console.log(first, second, sign)
+    if(key !== '.'){
+        if(second === '' && sign === ''){
+            first+=key;
+            calc.value +=key;
+            console.log(first, second, sign)
+        } else{
+            second+=key;
+            calc.value +=key;
+            console.log(first, second, sign)
+        }
     } else{
-        second+=key;
-        calc.value +=key;
-        console.log(first, second, sign)
+        if(first !== '' && !first.includes('.')){
+            first+=key;
+            calc.value +=key;
+            console.log(first, second, sign)
+        } else if(second !== '' && !second.includes('.')){
+            second+=key;
+            calc.value +=key;
+            console.log(first, second, sign)
+        }
     }
 }
 
@@ -35,40 +47,48 @@ function clickOperation(key){
     } else if(first!=='' && second!==''){
         switch(sign){
             case '÷':{
-                results = first/second;
+                if(+second === 0){
+                    calc.value = "Ошибка";
+                    first = '';
+                    second = '';
+                    sign = '';
+                    return;
+                }
+                first = first/second;
                 break;
             }
             case '%':{
-                results = first%second;
+                first = first/100*second;
                 break;
             }
-            case '✕':{
-                results = first*second;
+            case '×':{
+                first = first*second;
                 break;
             }
             case '+':{
-                results = +first+ +second;
+                first = +first+ +second;
                 break;
             }
             case '−':{
-                results = first-second;
+                first = first-second;
                 break;
             }
         }
-        calc.value = results;
+        calc.value = first;
         sign = key;
         key === '=' ? sign = '' : calc.value +=key;
-        first = results;
-        second = ''
-        console.log(first, second, sign)
+        second = '';
+        console.log(first, second, sign);
     } else if(key !== '=' && first !=='' && sign === ''){
         sign = key;
         calc.value += key;
         console.log(first, second, sign)
-    } else{
+    } else if(sign !== '' && key !== '='){
         sign = key;
         calc.value = calc.value.slice(0,-1);
         calc.value+=sign;
+        console.log(first, second, sign)
+    } else{
         console.log(first, second, sign)
     }
 }
@@ -76,6 +96,9 @@ function clickOperation(key){
 function buttonsClick(e){
     if(!e.target.classList.contains('btn')) return;
     let key = e.target.textContent;
+    if(first === '' && second === '' && sign === ''){
+        calc.value = '';
+    }
     if(number.includes(key)){
         clickNumber(key);
     } else if(operation.includes(key)){
