ccdf633222bdfff0f4734b336a70deb8d69be487
Angelika <angelika_romanovskaya_2002@mail.ru>
2023-08-03 11:37:05 +0300

    Add clear button

--- js/script.js
+++ js/script.js
@@ -5,61 +5,81 @@ const number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
 const operation = ['C', '÷', '%', '←', '✕', '−', '+', '=']
 let first = '', second = '', sign= '', results;
 
+function clickNumber(key){
+    if(second === '' && sign === ''){
+        first+=key;
+        calc.value +=key;
+        console.log(first, second, sign)
+    } else{
+        second+=key;
+        calc.value +=key;
+        console.log(first, second, sign)
+    }
+}
+
+function clickOperation(key){
+    if(key === 'C'){
+        first = '', second = '', sign= '', results ='';
+        calc.value = '';
+        console.log(first, second, sign)
+    }  else if(key === '←'){
+        if(second!== ''){
+            second = second.slice(0,-1);
+        } else if(sign!==''){
+            sign = '';
+        } else{
+            first = first.toString().slice(0,-1);
+        }
+        calc.value = calc.value.slice(0,-1);
+        console.log(first, second, sign)
+    } else if(first!=='' && second!==''){
+        switch(sign){
+            case '÷':{
+                results = first/second;
+                break;
+            }
+            case '%':{
+                results = first%second;
+                break;
+            }
+            case '✕':{
+                results = first*second;
+                break;
+            }
+            case '+':{
+                results = +first+ +second;
+                break;
+            }
+            case '−':{
+                results = first-second;
+                break;
+            }
+        }
+        calc.value = results;
+        sign = key;
+        key === '=' ? sign = '' : calc.value +=key;
+        first = results;
+        second = ''
+        console.log(first, second, sign)
+    } else if(key !== '=' && first !=='' && sign === ''){
+        sign = key;
+        calc.value += key;
+        console.log(first, second, sign)
+    } else{
+        sign = key;
+        calc.value = calc.value.slice(0,-1);
+        calc.value+=sign;
+        console.log(first, second, sign)
+    }
+}
+
 function buttonsClick(e){
     if(!e.target.classList.contains('btn')) return;
     let key = e.target.textContent;
     if(number.includes(key)){
-        if(second === '' && sign === ''){
-            first+=key;
-            calc.value +=key;
-            console.log(first, second, sign)
-        } else if(first!=='' && second!=='' && sign ===''){
-            calc.value +=key;
-            console.log(first, second, sign)
-        } else{
-            second+=key;
-            calc.value +=key;
-            console.log(first, second, sign)
-        }
+        clickNumber(key);
     } else if(operation.includes(key)){
-        if(key === 'C'){
-            first = '', second = '', sign= '', results ='';
-            calc.value = '';
-            console.log(first, second, sign)
-        } else if(first!=='' && second!==''){
-            switch(sign){
-                case '÷':{
-                    results = first/second;
-                    break;
-                }
-                case '%':{
-                    results = first%second;
-                    break;
-                }
-                case '✕':{
-                    results = first*second;
-                    break;
-                }
-                case '+':{
-                    results = +first+ +second;
-                    break;
-                }
-                case '−':{
-                    results = first-second;
-                    break;
-                }
-            }
-            calc.value = results;
-            sign = key;
-            key === '=' ? sign = '' : calc.value +=key;
-            first = results;
-            second = ''
-            console.log(first, second, sign)
-        } else{
-            sign = key;
-            calc.value += key;
-            console.log(first, second, sign)
-        }
+        clickOperation(key);
     }
 }
 
